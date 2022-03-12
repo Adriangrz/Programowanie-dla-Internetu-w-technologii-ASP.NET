6b80e67346fab698f7630f64ec69f4962cfff722
Adriangrz <adriang1412g@gmail.com>
2022-03-12 12:02:17 +0100

    fixes warning about price variable

--- NetworkOfShops/NetworkOfShops.Models/Product.cs
+++ NetworkOfShops/NetworkOfShops.Models/Product.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,7 +12,9 @@ namespace NetworkOfShops.Models
         public int Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
-        public float Price { get; set; }
+        [Range(1, 1000.99)]
+        [DataType(DataType.Currency)]
+        public decimal Price { get; set; }
         public int ShopId { get; set; }
         public Shop? Shop { get; set; }
         public ICollection<OrderDetails>? OrderDetails { get; set; }
